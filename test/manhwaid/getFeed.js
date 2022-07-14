63eb13fad4ac2ab4e534cdeef845bc8f69e1ef76
= <=>
2022-07-14 13:37:01 +0700

    sbsbww

--- test/manhwaid/getFeed.js
+++ test/manhwaid/getFeed.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const scraper = require('../../App/Scraper/Manhwaid.js');
 
 (async () => {
-  await scrape.start();
+  await scraper.start();
   
   const data = await scraper.getFeed();
   console.log(data);
   
-  await scrape.end();
+  await scraper.end();
 })();
